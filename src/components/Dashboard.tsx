6e871b7aae92134e5e8ef3c650e5e5ff0fd5c271
Sumit <sk990005@outlook.com>
2026-05-16 11:42:54 +0530

    Update Dashboard with redux state

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,13 @@
+import { useAppSelector } from "@/hooks/useActions"
 import { Box, Heading } from "@chakra-ui/react"
 
 const Dashboard = () => {
+  const user = useAppSelector((state) => state.onboarding.personalProfile.name)
+
   return (
     <Box>
       <Heading as="h2" fontSize="4xl">
-        Welcome, <u>John Doe</u> 👋🏻
+        Welcome, <u>{user}</u> 👋🏻
       </Heading>
     </Box>
   )
